ec11750e210d193559da6bfbeb46dfe7bc452b50
Vibhuti Pradeep <vibhutisai@gmail.com>
2019-11-18 17:15:06 +0800

    wip

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }.join(' ')
+  end
+
+  def score
+    url = "https://wagon-dictionary.herokuapp.com/#{params['attempt']}"
+    response = open(url).read
+    word = JSON.parse(response)
+      if word['found'] == true
+        @score = "Congratulations! #{params['attempt'].upcase} is a valid \n
+        English word!"
+      else
+        @score = "Sorry, but #{params['attempt'].upcase} does not seem to be a\n
+         valid English word..."
+      end
+  end
+end
